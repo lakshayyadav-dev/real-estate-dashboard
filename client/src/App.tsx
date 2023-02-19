7c4cd9f6cd7fcac445e213ab50bd7d727ad9df5f
Lakshay <lakshayyadav2006@gmail.com>
2023-02-19 18:49:03 +0530

    added prettier formating and converted client id to an environment variable.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import { Refine, AuthProvider } from "@pankod/refine-core";
 import {
-  notificationProvider,
-  RefineSnackbarProvider,
-  CssBaseline,
-  GlobalStyles,
-  ReadyPage,
-  ErrorComponent,
+    notificationProvider,
+    RefineSnackbarProvider,
+    CssBaseline,
+    GlobalStyles,
+    ReadyPage,
+    ErrorComponent,
 } from "@pankod/refine-mui";
 
 import dataProvider from "@pankod/refine-simple-rest";
@@ -22,101 +22,103 @@ import { parseJwt } from "utils/parse-jwt";
 
 const axiosInstance = axios.create();
 axiosInstance.interceptors.request.use((request: AxiosRequestConfig) => {
-  const token = localStorage.getItem("token");
-  if (request.headers) {
-    request.headers["Authorization"] = `Bearer ${token}`;
-  } else {
-    request.headers = {
-      Authorization: `Bearer ${token}`,
-    };
-  }
+    const token = localStorage.getItem("token");
+    if (request.headers) {
+        request.headers["Authorization"] = `Bearer ${token}`;
+    } else {
+        request.headers = {
+            Authorization: `Bearer ${token}`,
+        };
+    }
 
-  return request;
+    return request;
 });
 
 function App() {
-  const authProvider: AuthProvider = {
-    login: ({ credential }: CredentialResponse) => {
-      const profileObj = credential ? parseJwt(credential) : null;
+    const authProvider: AuthProvider = {
+        login: ({ credential }: CredentialResponse) => {
+            const profileObj = credential ? parseJwt(credential) : null;
 
-      if (profileObj) {
-        localStorage.setItem(
-          "user",
-          JSON.stringify({
-            ...profileObj,
-            avatar: profileObj.picture,
-          })
-        );
-      }
+            if (profileObj) {
+                localStorage.setItem(
+                    "user",
+                    JSON.stringify({
+                        ...profileObj,
+                        avatar: profileObj.picture,
+                    })
+                );
+            }
 
-      localStorage.setItem("token", `${credential}`);
+            localStorage.setItem("token", `${credential}`);
 
-      return Promise.resolve();
-    },
-    logout: () => {
-      const token = localStorage.getItem("token");
+            return Promise.resolve();
+        },
+        logout: () => {
+            const token = localStorage.getItem("token");
 
-      if (token && typeof window !== "undefined") {
-        localStorage.removeItem("token");
-        localStorage.removeItem("user");
-        axios.defaults.headers.common = {};
-        window.google?.accounts.id.revoke(token, () => {
-          return Promise.resolve();
-        });
-      }
+            if (token && typeof window !== "undefined") {
+                localStorage.removeItem("token");
+                localStorage.removeItem("user");
+                axios.defaults.headers.common = {};
+                window.google?.accounts.id.revoke(token, () => {
+                    return Promise.resolve();
+                });
+            }
 
-      return Promise.resolve();
-    },
-    checkError: () => Promise.resolve(),
-    checkAuth: async () => {
-      const token = localStorage.getItem("token");
+            return Promise.resolve();
+        },
+        checkError: () => Promise.resolve(),
+        checkAuth: async () => {
+            const token = localStorage.getItem("token");
 
-      if (token) {
-        return Promise.resolve();
-      }
-      return Promise.reject();
-    },
+            if (token) {
+                return Promise.resolve();
+            }
+            return Promise.reject();
+        },
 
-    getPermissions: () => Promise.resolve(),
-    getUserIdentity: async () => {
-      const user = localStorage.getItem("user");
-      if (user) {
-        return Promise.resolve(JSON.parse(user));
-      }
-    },
-  };
+        getPermissions: () => Promise.resolve(),
+        getUserIdentity: async () => {
+            const user = localStorage.getItem("user");
+            if (user) {
+                return Promise.resolve(JSON.parse(user));
+            }
+        },
+    };
 
-  return (
-    <ColorModeContextProvider>
-      <CssBaseline />
-      <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
-      <RefineSnackbarProvider>
-        <Refine
-          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-          notificationProvider={notificationProvider}
-          ReadyPage={ReadyPage}
-          catchAll={<ErrorComponent />}
-          resources={[
-            {
-              name: "posts",
-              list: MuiInferencer,
-              edit: MuiInferencer,
-              show: MuiInferencer,
-              create: MuiInferencer,
-              canDelete: true,
-            },
-          ]}
-          Title={Title}
-          Sider={Sider}
-          Layout={Layout}
-          Header={Header}
-          routerProvider={routerProvider}
-          authProvider={authProvider}
-          LoginPage={Login}
-        />
-      </RefineSnackbarProvider>
-    </ColorModeContextProvider>
-  );
+    return (
+        <ColorModeContextProvider>
+            <CssBaseline />
+            <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+            <RefineSnackbarProvider>
+                <Refine
+                    dataProvider={dataProvider(
+                        "https://api.fake-rest.refine.dev"
+                    )}
+                    notificationProvider={notificationProvider}
+                    ReadyPage={ReadyPage}
+                    catchAll={<ErrorComponent />}
+                    resources={[
+                        {
+                            name: "posts",
+                            list: MuiInferencer,
+                            edit: MuiInferencer,
+                            show: MuiInferencer,
+                            create: MuiInferencer,
+                            canDelete: true,
+                        },
+                    ]}
+                    Title={Title}
+                    Sider={Sider}
+                    Layout={Layout}
+                    Header={Header}
+                    routerProvider={routerProvider}
+                    authProvider={authProvider}
+                    LoginPage={Login}
+                />
+            </RefineSnackbarProvider>
+        </ColorModeContextProvider>
+    );
 }
 
 export default App;
